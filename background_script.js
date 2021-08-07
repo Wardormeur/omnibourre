c15c2376c3e8ebcddc8a8b3eb05ce75eeefc91cd
Wardormeur <wardormeur@gmail.com>
2021-08-07 17:09:27 +0100

    Rework data structure to allow for ordering of results

--- background_script.js
+++ background_script.js
@@ -5,45 +5,66 @@ browser.omnibox.setDefaultSuggestion({
 
 
 const definitions = [{
-  element: 'Tab',
-  actions: ['create', 'duplicate', 'close all', 'close right', 'close left'],
-  callbacks: {
-    create: () => browser.tabs.create({}),
-    duplicate: async () => {
-      const curr = await browser.tabs.query({currentWindow: true, active: true});
-      browser.tabs.create({ url: curr[0].url });
+  name: 'Tab',
+  callbacks: [
+    {
+      name: 'create', 
+      fn: () => browser.tabs.create({}),
     },
-    'close all': async () => {
-      let tabs = await browser.tabs.query({ currentWindow: true });
-      browser.tabs.remove(tabs.map(t => t.id));
+    { 
+      name: 'duplicate',
+      fn: async () => {
+        const curr = await browser.tabs.query({currentWindow: true, active: true});
+        browser.tabs.create({ url: curr[0].url });
+      }
     },
-    'close right': async () => {
-      let tabs = await browser.tabs.query({ currentWindow: true });
-      let tab = await browser.tabs.query({ currentWindow: true, active: true });
-      let filteredTabs = tabs.filter((_t) => _t.index > tab[0].index)
-      browser.tabs.remove(filteredTabs.map(t => t.id));
+    { 
+      name: 'close all',
+      fn: async () => {
+        let tabs = await browser.tabs.query({ currentWindow: true });
+        browser.tabs.remove(tabs.map(t => t.id));
+      },
     },
-    'close left': async () => {
-      let tabs = await browser.tabs.query({ currentWindow: true });
-      let tab = await browser.tabs.query({ currentWindow: true, active: true });
-      let filteredTabs = tabs.filter((_t) => _t.index < tab[0].index)
-      console.log(filteredTabs);
-      browser.tabs.remove(filteredTabs.map(t => t.id));
+    { 
+      name: 'close all to the right',
+      fn: async () => {
+        let tabs = await browser.tabs.query({ currentWindow: true });
+        let tab = await browser.tabs.query({ currentWindow: true, active: true });
+        let filteredTabs = tabs.filter((_t) => _t.index > tab[0].index)
+        browser.tabs.remove(filteredTabs.map(t => t.id));
+      },
     },
-  },
-}]
+    { 
+      name: 'close all to the left',
+      fn: async () => {
+        let tabs = await browser.tabs.query({ currentWindow: true });
+        let tab = await browser.tabs.query({ currentWindow: true, active: true });
+        let filteredTabs = tabs.filter((_t) => _t.index < tab[0].index)
+        console.log(filteredTabs);
+        browser.tabs.remove(filteredTabs.map(t => t.id));
+      },
+  }],
+}];
 
-const keys = ['element', 'actions'];
-const fuse = new Fuse(definitions, { keys });
+function flattenDefinitions(definitions) {
+  return definitions.flatMap(element => {
+    return element.callbacks.map(rule => {
+      rule.element = element.name; 
+      return rule;
+    });
+  });
+}
+
+const keys = ['name' , 'element'];
+const fuse = new Fuse(flattenDefinitions(definitions), { keys });
 function listener(input, suggest) {
   let res = fuse.search(input, { includeScore: true });
-  let found = res[0].item;
-  let suggestions = found.actions.map((a) => ({ content: `${found.element}.${a}`, description: `${found.element} ${a}` }));
+  let suggestions = res.map((a) => ({ content: `${a.item.element}-${a.item.name}`, description: `${a.item.name}` }));
   suggest(suggestions);
 }
 browser.omnibox.onInputChanged.addListener(listener)
 
 browser.omnibox.onInputEntered.addListener((url, disposition) => {
-  let [element, action, rest] = url.split('.');
-  definitions.find((d) => d.element == element).callbacks[action]();
+  let [element, action, rest] = url.split('-');
+  definitions.find((d) => d.name == element).callbacks.find(c => c.name === action).fn();
 });
